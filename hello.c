1798d5e7ad8e6c41af971d51d8c9eb08d486bc2e
Katelyn Breitbarth <kbreitbarth2@huskers.unl.edu>
2021-08-26 17:49:33 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Katelyn Breitbath
+ * Date: 2021/08/26
+ *
+ * A program to print my name.
+ *
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv) {
+
+        printf("Katelyn Breitbarth\n");
+
+        return 0;
+ }
